5dc947ed14c391aafa146e2c99f88383aa96f07a
Muhammad SA <MuhammadSA211999>
2023-05-09 12:14:17 +0600

    generic arrow function

---
+++ src/generic-func.ts
@@ -0,0 +1,38 @@
+const createArray = (param: string) => {
+    return [param]
+}
+const result1 = createArray('BD')
+
+const createArray1 = (param: number): number[] => [param]
+const result2 = createArray1(3)
+
+
+const genericFunc1 = <T>(param: T): T[] => {
+    return [param]
+}
+const genRes1 = genericFunc1<string>('BD')
+
+interface UserIn {
+    name: string;
+    age: number
+}
+const user3: UserIn = {
+    name: 'MSA',
+    age: 24
+}
+const genRes2 = genericFunc1<UserIn>(user3)
+
+const genFunTuple = <X, Y>(par1: X, par2: Y): [X, Y] => [par1, par2]
+const genFunTupleRes1 = genFunTuple<{ name: string }, string>({ name: 'MSA' }, 'Gulbahar')
+
+interface UserIn2 {
+    role: string;
+    name: string;
+    age: number
+}
+const user4: UserIn2 = {
+    role: 'hus',
+    name: 'MSA',
+    age: 24
+}
+const genFunTupleRes2 = genFunTuple<UserIn, UserIn2>(user3, user4)
